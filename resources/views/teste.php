079736006683990876a94de6a851ec3f35ea3848
WillianGraciani <willian.graciani@hotmail.com>
2020-03-10 21:49:41 -0300

    Aula2

---
+++ resources/views/teste.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Document</title>
+</head>
+<body style="margin:200px;">
+    <div style="text-align:center; border:groove; padding:20px;">
+        <form action='' style="text-align:left;">
+            <br><br>
+            <label>
+                Nome:
+            </label>
+            <input type="text" id="nome" name="nome">
+            <label >
+                CPF:
+            </label>
+            <input type="text" id="cpf" name="cpf"><br><br>
+            <label>
+                Endereço:
+            </label>
+            <input type="text" id="end" name="end"><br><br>
+            <input type="submit" value="submit"><br><br>
+        </form>
+    </div>
+</body>
+</html>
